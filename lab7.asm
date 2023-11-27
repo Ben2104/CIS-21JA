570b9d960df5618ed470e690ceddc8e2ebc024d2
Khoi Hoang Do <dohoangkhoi341@gmail.com>
2023-11-26 21:58:33 -0800

    Add files via upload

--- lab7.asm
+++ lab7.asm
@@ -5,7 +5,7 @@ INCLUDE Irvine32.inc
 
 mWriteStr MACRO buffer
 	push edx
-	mov edx, offset buffer
+	mov edx, buffer
 
 	call writestring
 	call crlf
@@ -31,13 +31,27 @@ mainLoop:
 	push offset negative_error
 	push offset overflow_error
 
-
 	call readInput
 	pop eax			;retrieve the valid integer from the readInput
 	
+	;eax and edi will be saved to use in the convert procedure
 	mov edi, offset hexString
+	mov edx, offset continue
 	call convert
 	
+	COMMENT !
+	for    (ecx = 0; ecx < 4; ecx++){
+		set ebx = 0
+		if (hexString[ebx] == '0'){
+			mov hexString[ebx], 8h	to remove the 0s
+			ebx++
+		}
+	}
+	8h in ascii table is Backspace
+	we also can use 8 decimal as well
+!
+	mWriteStr edi			;print hexString before clear the zero
+
 	mov ecx, 4
 	mov ebx, 0
 ;Check if there is 0s lead and erase it
@@ -50,29 +64,21 @@ hexLoop:
 	next:
 		loop hexLoop
 
-	mWriteStr hexString
-	mWriteStr continue
+	mWriteStr edi
+	mWriteStr edx
 	call readchar
 	call writeChar
 	call crlf
 	
 
-	mov dword ptr [hexString], '0000'	;reset the hexString array
+	mov dword ptr [edi], '0000'	;reset the hexString array for the next prompt
 	
-	cmp al, 'y'
-	je mainLoop
-	cmp al, 'Y'
-	je mainLoop
+;	jump to mainLoop
 
-COMMENT !
-	for (ecx = 0; ecx < 4; ecx++){
-		set ebx = 0
-		if (hexString[ebx] == '0'){
-			mov hexString[ebx], 8h	to remove the 0s
-			ebx++
-		}
-	}
-!
+	cmp al, 'y'			;if 1st condition is right jump to mainLoop	
+	je mainLoop			;if 1st condition is wrong go to 2nd codition 
+	cmp al, 'Y'			;if 2nd condition is right jump to mainLoop
+	je mainLoop			;if 2nd condition is wrong go to exit
 
 
 	exit	
@@ -100,7 +106,7 @@ readInput PROC
 ;[ebp + 8] => overflow_error
 
 Prompt_user:
-	mWriteStr prompt
+	mWriteStr [ebp + 16]
 	call readInt
 	 
 	cmp eax, 0		;if the input is negative jump to negativeError
@@ -108,20 +114,18 @@ Prompt_user:
 	cmp eax, 65535
 	ja overflowError; if the input is over 2^16 jump to overflowError
 	
-	mov [ebp + 20], eax
+	mov [ebp + 20], eax		;mov eax to the return address
 	pop eax
 	pop ebp
-
-
 
 	ret 4*3
 
 overflowError:
-	mWriteStr overflow_error
+	mWriteStr [ebp + 8]
 	jmp Prompt_user
 
 negativeError:
-	mWriteStr negative_error
+	mWriteStr [ebp + 12]
 	jmp Prompt_user
 
 readInput ENDP
@@ -132,19 +136,23 @@ convert PROC
 ; masking with 1111 retrieves the remainder
 ; eax/= 16
 
-	
-	
+		mov ecx, 3
 	convertLoop:
 		mov ebx, eax
-		and ebx, 0Fh
-		shr eax, 4
+		and ebx, 0Fh	;ebx arguments will be used for toChar
+						;and ebx, 0Fh will give me the remainder of eax % 16
+		shr eax, 4		;shift right 4 mean eax / 2^4 = eax 16
 
 		call toChar
 		
 		;set ecx to 3
-		mov ecx, 3
-		mov [edi + ecx], bl		;add edi + 3 to store the hexstring to the 3rd element and decrement edi until eax = 0 		
-		dec edi
+		;hexString[3] = bl
+		;hexString[2] = bl
+		;hexString[1] = bl
+		;hexString[0] = bl
+		
+		mov [edi + ecx], bl		;add edi + 3 to store the hexstring to the 4rd element and decrement edi until eax = 0 		
+		dec ecx
 
 		test eax, eax			;test if eax is 0 
 		jnz convertLoop
@@ -166,7 +174,7 @@ toChar PROC
 	;}
 	ifBlock:
 		add bl, 30h
-		jmp break
+		jmp break		;jump over the elseBlock
 	elseBlock:
 		add bl, 37h
 
